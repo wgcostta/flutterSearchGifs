b0f860c1bd4dfc20b2e43b6a4551900d1f648cb8
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-26 14:56:15 -0300

    Migrando repositórios

---
+++ android/app/src/main/kotlin/br/com/cloud99s/flutter_search_gifs/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cloud99s.flutter_search_gifs
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
